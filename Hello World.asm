dfca18aa05547b6928dff85d6c97c51d694f8e51
vsadygv <vugarsadygov1@gmail.com>
2020-08-08 20:09:24 -0500

    memory segments in comments

--- Hello World.asm
+++ Hello World.asm
@@ -14,3 +14,17 @@ _start:				;starts program
 section	.data			;data section is used for declaring initialized data or constants
 msg db 'Hello, World!',	0xa	;string to be printed
 len equ $ - msg			;length of the string
+
+
+
+; memory segments
+;
+;
+;    Data segment − It is represented by .data section and the .bss. The .data section is used to declare the memory region, where data elements are stored for the program. This section cannot be expanded after the data elements are declared, and it remains static throughout the program.
+;
+;    The .bss section is also a static memory section that contains buffers for data to be declared later in the program. This buffer memory is zero-filled.
+;
+;    Code segment − It is represented by .text section. This defines an area in memory that stores the instruction codes. This is also a fixed area.
+;
+;    Stack − This segment contains data values passed to functions and procedures within the program.
+;
